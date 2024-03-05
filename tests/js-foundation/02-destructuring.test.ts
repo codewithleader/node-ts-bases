35eadf49b6e202d760cd776b332abd294dca4d13
Elis Antonio Perez <elisperezmusic@gmail.com>
2024-03-04 23:28:36 -0500

    test(destructurin): :test_tube: Test in destructuring

---
+++ tests/js-foundation/02-destructuring.test.ts
@@ -0,0 +1,15 @@
+import { characters } from '../../src/js-foundation/02-destructuring';
+
+describe('02-destructuring', () => {
+  test('characters should contain Flash and Superman ', () => {
+    expect(characters).toContain('Flash');
+    expect(characters).toContain('Superman');
+  });
+
+  test('First character should be Flash, and second Superman', () => {
+    const [flash, superman] = characters;
+
+    expect(flash).toBe('Flash');
+    expect(superman).toBe('Superman');
+  });
+});
