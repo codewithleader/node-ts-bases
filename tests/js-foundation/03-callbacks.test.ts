289d3518b91686e9a6d4ea54841e993a3d32bb64
Elis Antonio Perez <elisperezmusic@gmail.com>
2024-03-05 00:49:08 -0500

    test: :test_tube: done
    argumento done para esperar que termine la funcion de ejecutarse

--- tests/js-foundation/03-callbacks.test.ts
+++ tests/js-foundation/03-callbacks.test.ts
@@ -1,12 +1,14 @@
 import { getUserById } from '../../src/js-foundation/03-callbacks';
 
 describe('Test 03-callbacks', () => {
-  test('getUserById should return an error if user does not exist', () => {
-    const id = 10; // No existe usuario con id = 10.
+  test('getUserById should return an error if user does not exist', (done) => {
+    const id = 10; // No user with id = 10 exists.
 
     getUserById(id, (error, user) => {
       expect(error).toBe(`User not found with id ${id}`);
-      expect(user).toBe(undefined); // El usuario no existe.
+      expect(user).toBe(undefined); // The user does not exist.
+
+      done(); // Indicates that the test has completed.
     });
   });
 });
